1c1e51603f413c83d38d6a359d934c54b13ba229
pfeffer629 <pfeffer629@gmail.com>
2014-12-27 13:57:27 -0800

    added font and working buttons

--- javascript/script.js
+++ javascript/script.js
@@ -2,20 +2,58 @@ $(document).ready(function() {
 	var height = screen.height
 
   $(".blog_button").on("click", function(e){
- 		e.preventDefault();
-  	$('#primary').fadeOut();
-		$('#blog').css({
-			"visibility" : "visible",
-			"height" : height
-		})
-		$('.container').css("height", height)
-
-  	$('.container').animate({
-  		width:"100%"
-  	}, 1500, function(){
-  		$("#blog").animate({opacity: 1})
-  	})
+    e.preventDefault();
+    $('#blog').css("visibility", "visible")
+    $('#primary').fadeOut(800, function(){
+      $("#blog").animate({opacity: 1})
+    });
   })
+
+  $(".portfolio_button").on("click", function(e){
+    e.preventDefault();
+    $('#portfolio').css("visibility", "visible")
+    $('#primary').fadeOut(800, function(){
+      $("#portfolio").animate({opacity: 1})
+    });
+  })
+
+  $(".about_button").on("click", function(e){
+    e.preventDefault();
+    $('#about').css("visibility", "visible")
+    $('#primary').fadeOut(800, function(){
+      $("#about").animate({opacity: 1})
+    });
+  })
+
+  $(".contact_button").on("click", function(e){
+    e.preventDefault();
+    $('#contact').css("visibility", "visible")
+    $('#primary').fadeOut(800, function(){
+      $("#contact").animate({opacity: 1})
+    });
+  })
+
+  $(".return").on("click", function(e){
+    e.preventDefault();
+    // $(document).css("background-color", "red")
+    $(".return").closest("div").animate({opacity: 0}, function(){
+      $('#primary').fadeIn();
+    });
+  })
+
+    //attempt at sliding animation
+		// $('body').css({'background':'red no-repeat center center fixed', 
+    //   '-webkit-background-size':'cover',
+    //   '-moz-background-size':'cover',
+    //   '-o-background-size':'cover',
+    //   'background-size':'cover'})
+  	// $('.blog_container').animate({
+  	// 	width:"100%"
+  	// }, 1500, function(){
+  		// $("#blog").animate({opacity: 1})
+  	// })
+  // })
 
 
 });
+
