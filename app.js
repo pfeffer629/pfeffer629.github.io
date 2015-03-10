fd480250ce1e058686cc3115cdf1cecaf970bbdb
pfeffer629 <pfeffer629@gmail.com>
2015-03-09 19:39:19 -0700

    ajax load content

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-$(document).ready(function() {
+$(document).ready(function(){
+ 	$("#about").click(function(){
+ 		$('#result').load('pages/about.html');
+	});
+
+	$("#portfolio").click(function(){
+ 		$('#result').load('pages/portfolio.html');
+	});
+
+	$("#resume").click(function(){
+ 		$('#result').load('pages/resume.html');
+	});
+
+	$("#blog").click(function(){
+ 		$('#result').load('pages/blog.html');
+	});
+
 });
