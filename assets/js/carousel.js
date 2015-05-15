ac2acde811c49fbf49eee440ee10669fe3ae5d5e
Mark Pfeffer <pfeffer629@gmail.com>
2015-05-14 17:42:36 -0700

    quantum

---
+++ assets/js/carousel.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    autoplay: true,
+    dots: true,
+    pauseOnHover: false,
+    swipeToSlide: true,
+    fade: true,
+    speed: 1000
+  });
+});
